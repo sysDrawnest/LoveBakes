7b21cc3c3568809e5710c240be239421f3442bde
SYS <118187096+sysDrawnest@users.noreply.github.com>
2026-04-27 13:52:33 +0530

    delete

--- backend/routes/adminRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getAnalytics, getAllUsers } from '../controllers/adminController.js';
-import { protect, admin } from '../middleware/authMiddleware.js';
-
-const router = express.Router();
-router.get('/analytics', protect, admin, getAnalytics);
-router.get('/users', protect, admin, getAllUsers);
-export default router;
